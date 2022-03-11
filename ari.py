2c688641e9632e3e6d7f08e38503cc0aa277abdb
sooraj <sooraj@hamon.in>
2022-03-11 15:30:55 +0530

    changed function names in file

--- ari.py
+++ ari.py
@@ -1,3 +1,4 @@
+import math
 def ari_emptychar(s):
     if len(s)==0:
         return 0
@@ -26,4 +27,23 @@ def ari_sentences(s):
      for i in s:
           if i == '!' or i == '?' or i == '.':
                acc+=1
-     return acc                              
+     return acc             
+def ari_calculation(string):
+     index= math.ceil(4.71*(ari_numchar(string)//ari_numwords(string))+0.5*(ari_numwords(string)//ari_sentences(string))-21.43)
+     dict={
+          1:"5-6 Kindergarten",
+          2:"6-7 First Grade",
+          3:"7-8 Second Grade",
+          4:"8-9 Third Grade",
+          5:"9-10 Fourth Grade",
+          6:"10-11 Fifth Grade",
+          7:"11-12 Sixth Grade",
+          8:"12-13 Seventh Grad",
+          9:"13-14 Eighth Grade",
+          10:'14-15 Ninth Grade',
+          11:'15-16 Tenth Grade',
+          12:'16-17 Eleventh Grade',
+          13:'17-18 Twelfth Grade',
+          14:'18-22 College student'
+          }
+     return dict[index]                      
