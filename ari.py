d0f13abc7e2e76a1b7faf9d63a7dd7968ee6dc5a
sooraj <sooraj@hamon.in>
2022-03-11 12:52:41 +0530

    writtened functions and test cases for ari characters,words and sentences

--- ari.py
+++ ari.py
@@ -1,3 +1,29 @@
 def ari_emptychar(s):
     if len(s)==0:
         return 0
+def ari_charalpha(s):
+    if not s.isalnum():
+        return False  
+def ari_numchar(s):
+    acc=0
+    for i in s:
+        acc+=1    
+    return acc     
+def ari_emptyword(s):
+     if " " not in s:
+          return None
+def ari_numwords(s):
+     acc=0
+     for i in s:
+          if i ==' ':
+               acc+=1
+     return acc
+def ari_emptysentence(s):
+     if "." or "!" or"?" not in s:
+          return None         
+def ari_sentences(s):
+     acc=0
+     for i in s:
+          if i == '!' or i == '?' or i == '.':
+               acc+=1
+     return acc                              
--- test_ari.py
+++ test_ari.py
@@ -2,4 +2,25 @@ import ari
 def test_ari_emptychar():
     ret = ari.ari_emptychar('')
     assert ret == 0
+def test_ari_charalpha():
+    ret =ari.ari_charalpha('#$!&')
+    assert ret == False
+def test_ari_numchar():
+    ret = ari.ari_numchar('1apqhgkl54')
+    assert ret ==  10   
+
+def test_ari_emptyword():
+    ret=ari.ari_emptyword("pqrst")
+    assert ret == None
+def test_ari_numwords():
+     ret=ari.ari_numwords('i am passionate about programming')
+     assert ret==4
+
+def test_ari_emptysentence():
+     ret=ari.ari_emptysentence('asdasfda pqrstuvwazjdjd')
+     assert ret==None
+def test_ari_sentence():
+     ret=ari.ari_sentences('how are you? where are you? dont feel bad. Study well!')
+     assert ret==4  
+
 
